6be30e7c6f11f4cb41401cd857c9eeea35bd06d9
best-developer-in-africa <wilsonajikwu12@gmail.com>
2022-02-02 11:02:14 +0100

    Control Flow

--- 2. Expressions, Variables & Constants/expressions_variables_constants/bin/statement.dart
+++ 2. Expressions, Variables & Constants/expressions_variables_constants/bin/statement.dart
@@ -3,4 +3,22 @@ void main() {
   print('My name is Wilson Chinedu');
 
   print('I am 31 years old');
+  final hoursSinceMidnight = DateTime.now().hour;
+  print(hoursSinceMidnight);
+
+  var counter = 0;
+  counter += 10; // counter = counter + 1;
+  print(counter);
+
+  counter -= 20; //counter = counter - 1;
+  print(counter);
+
+  double myValue = 10;
+  myValue *= 3; // myValue = myValue * 3;
+  print(myValue);
+  myValue /= 2; // myValue = myValue / 2
+
+  var addition = 10;
+  addition--;
+  print(addition);
 }
