13e2d9d0f5e2db9069f58829ede37ea44d5a3b37
icoweb2494 <icoweb2494@gmail.com>
2022-02-04 11:54:39 +0100

    assignment update

--- 4. Control Flow/control_flow/bin/control_flow.dart
+++ 4. Control Flow/control_flow/bin/control_flow.dart
@@ -1,4 +1,15 @@
 void main() {
-  const score = 93;
-  String message;
+  const attendance = 0.2;
+  const homework = 0.3;
+  const exam = 0.5;
+
+  const attendancePoints = 90;
+  const homeWorkPoints = 80;
+  const examPoints = 94;
+
+  const attendancepoints = attendance * attendancePoints;
+  const homeworkpoints = homework * homeWorkPoints;
+  const exampoints = exam * examPoints;
+  final totalgrade = attendancepoints + homeworkpoints + exampoints;
+  print(totalgrade.toInt());
 }
