2796c87199dc84a330ef5fb3fa38d08f7607467e
icoweb2494 <icoweb2494@gmail.com>
2022-02-04 11:16:39 +0100

    last class we stopped on if statemen hence the update

--- 4. Control Flow/control_flow/bin/control_flow.dart
+++ 4. Control Flow/control_flow/bin/control_flow.dart
@@ -1,14 +1,4 @@
 void main() {
-  const trafficLight = 'yellow';
-  var command = '';
-  if (trafficLight == 'red') {
-    command = 'Stop';
-  } else if (trafficLight == 'yellow') {
-    command = 'Slow down';
-  } else if (trafficLight == 'green') {
-    command = 'Go';
-  } else {
-    command = 'INVALID COLOR!';
-  }
-  print(command);
+  const score = 93;
+  String message;
 }
