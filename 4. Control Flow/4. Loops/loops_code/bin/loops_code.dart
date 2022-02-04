029e66df7ba1f0be0eb92b8319d850b9efb07248
icoweb2494 <icoweb2494@gmail.com>
2022-02-04 13:59:34 +0100

    loop class

---
+++ 4. Control Flow/4. Loops/loops_code/bin/loops_code.dart
@@ -0,0 +1,26 @@
+// import 'dart:math';
+
+void main() {
+  // var sum = 1;
+  // while (true) {
+  //   sum++;
+  //   if (sum == 100000000000) {
+  //     print(sum);
+  //     break;
+  //   }
+  // }
+
+//   final random = Random();
+//   var rng = random.nextInt(6) + 1;
+//   print(rng);
+
+//   while (rng != 6) {
+//     print("Not a six!");
+//     rng = random.nextInt(6) + 1;
+//   }
+//   print("Finally, you got a six");
+// }
+  for (var i = 0; i < 5; i++) {
+    print(i);
+  }
+}
