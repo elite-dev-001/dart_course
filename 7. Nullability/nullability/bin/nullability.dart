2c6f2a0a9c4f11a0cb47b9e469f8c6eb9840baf1
best-developer-in-africa <wilsonajikwu12@gmail.com>
2022-02-16 11:10:58 +0100

    Collections

--- 7. Nullability/nullability/bin/nullability.dart
+++ 7. Nullability/nullability/bin/nullability.dart
@@ -1,4 +1,11 @@
 void main() {
-  String? x;
-  print(x);
+  String? message;
+  final text = message ?? 'Error';
+  print(text);
+
+  double? fontSize;
+  // fontSize = fontSize ?? 20.0;
+  fontSize ??= 20.0;
+
+  // int 
 }
