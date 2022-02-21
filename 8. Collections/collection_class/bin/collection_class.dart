c70aff1517569d1a274b712b7013dc177efdb4df
icoweb2494 <icoweb2494@gmail.com>
2022-02-21 13:52:05 +0100

    abstract class and it's sub classes

--- 8. Collections/collection_class/bin/collection_class.dart
+++ 8. Collections/collection_class/bin/collection_class.dart
@@ -1,5 +1,98 @@
 void main() {
-  const numbers = [2, 3, 4, 5];
-  final squares = numbers.map((number) => number * number);
-  print(squares.toSet());
+  final jon = Person('Jon', 'Snow');
+  final jane = Student('Jane', 'Snow');
+  print(jon.fullName);
+  print(jane.fullName);
+  final historyGrade = Grade.B;
+  jane.grades.add(historyGrade);
+
+  final child = SomeChild();
+  child.doSomeWork();
+
+  final jessie = SchoolBandMember('Jessie', 'Jones');
+  final marty = StudentAthlete('Marty', 'McFly');
+
+  final students = <Student>[jane, jessie, marty];
+
+  //Abstract class instationtion usng a sub class and ading two overrrides.
+
+  Animal platypus = Platypus();
+  print(platypus.isAlive);
+  platypus.eat();
+  // platypus.layEggs();
+  platypus.move();
+  print(Platypus);
+}
+
+enum Grade { A, B, C, D, F }
+
+class Person {
+  Person(this.givenName, this.surname);
+
+  String givenName;
+  String surname;
+  String get fullName => '$givenName $surname';
+
+  @override
+  String toString() => fullName;
+}
+
+class Student extends Person {
+  Student(String givenName, String surname) : super(givenName, surname);
+  var grades = <Grade>[];
+  @override
+  String get fullName => '$surname, $givenName';
+}
+
+class SomeParent {
+  void doSomeWork() {
+    print('parent working');
+  }
+}
+
+class SomeChild extends SomeParent {
+  @override
+  void doSomeWork() {
+    super.doSomeWork();
+    print('child doing some other work');
+  }
+}
+
+class SchoolBandMember extends Student {
+  SchoolBandMember(String givenName, String surname)
+      : super(givenName, surname);
+  static const minimumPracticeTime = 2;
+}
+
+// Same simbling because they are dereining from //same parent called Student
+class StudentAthlete extends Student {
+  StudentAthlete(String givenName, String surname) : super(givenName, surname);
+  bool get isEligible => grades.every((grade) => grade != Grade.F);
+}
+
+abstract class Animal {
+  bool isAlive = true;
+  void eat();
+  void move();
+
+  @override
+  String toString() {
+    return "i'm a $runtimeType";
+  }
+}
+
+class Platypus extends Animal {
+  @override
+  void eat() {
+    print('Munch munch');
+  }
+
+  @override
+  void move() {
+    print('Glide glide');
+  }
+
+  void layEggs() {
+    print('Plop plop');
+  }
 }
